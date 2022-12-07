f58107655aa47f3a8cb553f9022558fa47c2cb0f
Rafaela Silvério <rafaelapetelin@gmail.com>
2022-12-07 18:14:00 -0300

    Projeto curso 2

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -4,11 +4,11 @@ export class Negociacoes{
     private negociacoes: Negociacao[] = [];
                         //^ é a mesma coisa que Array<Negociacao>
 
-    adiciona(negociacao: Negociacao){
+    public adiciona(negociacao: Negociacao){
         this.negociacoes.push(negociacao);
     }
 
-    lista(): readonly Negociacao[]{
+    public lista(): readonly Negociacao[]{
             //^ é a mesma coisa que ReadonlyArray<Negociacao>
         return this.negociacoes;
     }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,12 +2,10 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
-    //^ é a mesma coisa que Array<Negociacao>
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
     lista() {
-        //^ é a mesma coisa que ReadonlyArray<Negociacao>
         return this.negociacoes;
     }
 }
